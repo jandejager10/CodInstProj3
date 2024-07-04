816af16bf8621cf15f01bf77a09f2ff8efcc1535
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-07-04 08:40:17 +0100

    base.html and app.py started

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import *
+from forms import *
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
